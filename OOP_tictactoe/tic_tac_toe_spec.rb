67955c45a283089e8a5231be58164a0ffbca2d38
David Cole <misterdavemeister@gmail.com>
2015-10-17 15:02:04 -0500

    update spec file

--- OOP_tictactoe/tic_tac_toe_spec.rb
+++ OOP_tictactoe/tic_tac_toe_spec.rb
@@ -2,41 +2,40 @@ require 'tic_tac_toe'
 
 describe Game do
   before(:all) do
-    @game = Game.new
+    $game = Game.new(1, 2) #ai first, player second
   end
   
   it "initializes game" do
-    expect(@game.instance_of?(Game)).to eq(true)
+    expect($game.instance_of?(Game)).to eq(true)
   end
   
   it "creates a board (array) of 9 empty strings" do 
-    expect(@game.board).to eq(['', '', '', '', '', '', '', '', ''])
+    expect($game.board).to eq(['', '', '', '', '', '', '', '', ''])
   end
   
   it "creates an ai player" do
-    expect(@game.instance_variable_defined?(:@ai)).to eq(true)
+    expect($game.instance_variable_defined?(:@ai)).to eq(true)
   end
   
   it "creates a human player" do
-    expect(@game.instance_variable_defined?(:@player)).to eq(true)
+    expect($game.instance_variable_defined?(:@player)).to eq(true)
   end
   
   it "starts AI with xs" do
-    expect(@game.ai.piece).to eq("x")
+    expect($game.ai.piece).to eq("x")
   end
   
   it "starts human player with os" do
-    expect(@game.player.piece).to eq("o")
+    expect($game.player.piece).to eq("o")
   end
   
   it "can draw the board" do
-    @game.board = ['', '', '', '', '', '', '', '', '']
-    expect(@game.display)
+    $game.board = ['', '', '', '', '', '', '', '', '']
+    expect($game.display)
     puts
-    @game.board = ['o', 'o', 'o', 'x', 'x', 'x', 'o', 'x', 'o']
-    expect(@game.display)
-  end
-  
-  
-  
+    $game.board = ['o', 'o', 'o', 'x', 'x', 'x', 'o', 'x', 'o']
+    expect($game.display)
+    $game.board = ['', 'o', '', 'x', 'x', '', 'o', '', 'o']
+    expect($game.display_board)
+  end 
 end
