ca7961d52ccc8219709c96543f9ad55138a9804b
David Cole <misterdavemeister@gmail.com>
2015-10-18 07:54:40 -0500

    cleaned files up, preparing for second player

--- OOP_tictactoe/tic_tac_toe.rb
+++ OOP_tictactoe/tic_tac_toe.rb
@@ -231,3 +231,17 @@ class Game
   end
 end
 
+game = Game.start
+Game.clear 
+if !Game.game_over?
+  puts game.player.turn == 1 ? "You won the coin toss! You have first turn." : "You lost the coin toss! You have second turn."
+else
+  print "Goodbye!"
+end
+
+while !Game.game_over?  
+  game.play_round
+  Game.clear
+  print "Goodbye!" if Game.game_over?
+end
+
