9b36abe061ca4e32c2c1761f96108e1a4251b44b
David Cole <misterdavemeister@gmail.com>
2015-11-09 21:36:07 -0600

    has fibs and fibs_rec

--- recursion/fib/fib.rb
+++ recursion/fib/fib.rb
@@ -1,6 +1,13 @@
-def fib(n, start = 0, add = 1)
-  n == 1 ? add : fib(n - 1, add, start + add)
-end 
+def fibs(n, start = 0, add = 1)
+  n.times { |num| num == n - 1 ? (return add) : (start, add = add, start + add) }
+end
 
-puts fib(8) #=> 21
-puts fib(20) #=> 6765
+def fibs_rec(n, start = 0, add = 1)
+  n == 1 ? add : fibs_rec(n - 1, add, start + add)
+end
+
+puts fibs(8) #=> 21
+puts fibs(20) #=> 6765
+
+puts fibs_rec(8) #=> 21
+puts fibs_rec(20) #=> 6765
