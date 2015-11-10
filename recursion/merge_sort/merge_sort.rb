9fa953849890177358f1197212e2295a69e835f0
David Cole <misterdavemeister@gmail.com>
2015-11-10 16:56:11 -0600

    merge_sort refactored

--- recursion/merge_sort/merge_sort.rb
+++ recursion/merge_sort/merge_sort.rb
@@ -1,30 +1,21 @@
 def merge_sort(arr)
-  c = arr.count - 1
-  half = (c / 2)
-  return arr if c == 0
-  if arr.count > 1
-    first = arr[0..half]
-    second = arr[half+1..c]
-    first = merge_sort(first)
-    second = merge_sort(second)
-    merged = merge(first, second)
+  c = arr.count
+  return arr if c == 1
+  half = ((c - 1) / 2)
+  first = merge_sort(arr[0..half])
+  second = merge_sort(arr[half+1..c-1])
+  merged = []
+  total = first.count + second.count
+  while merged.count < total
+    if first[0].nil? && !second[0].nil?
+      merged << second.shift
+    elsif second[0].nil? && !first[0].nil?
+      merged << first.shift
+    else
+      merged << ( first[0] < second[0] ?  first.shift : second.shift )
+    end
   end
   merged
 end
 
-def merge(first, second)
-  new_arr = []
-  total = first.count + second.count
-  while new_arr.count < total
-    if first[0].nil? && !second[0].nil?
-      new_arr << second.shift
-    elsif second[0].nil? && !first[0].nil?
-      new_arr << first.shift
-    else
-      first[0] < second[0] ? new_arr << first.shift : new_arr << second.shift
-    end
-  end
-  new_arr
-end
-
-puts merge_sort([4, 3, 7, 1, 9, 2, 8, 5, 6])
+puts merge_sort([6, 4, 3, 7, 1, 9, 2, 8, 5, 0, -99, -2005])
